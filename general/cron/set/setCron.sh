3665881140eae2397218798e4abd40fb09ea9230
Minor Solis <minorsolis@gmail.com>
2016-05-18 09:42:36 -0600

    Adding documentation

--- general/cron/set/setCron.sh
+++ general/cron/set/setCron.sh
@@ -1,1 +1,1 @@
-* * * * * {edit_here_path_to_your_installation}/ec2InstanceBackup/general/cron/every1Day.sh
+0 1 * * * {edit_here_path_to_your_installation}/ec2InstanceBackup/general/cron/every1Day.sh
